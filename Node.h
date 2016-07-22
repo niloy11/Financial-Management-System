e28df8eb617217997eca81bea642d6978b8a49ee
niloy11 <niloyeee147@gmail.com>
2016-07-22 08:27:09 -0400

    Created Node.h

---
+++ Node.h
@@ -0,0 +1,30 @@
+//
+//	Node.h
+//	construct node to store information for stock symbol and number of stocks
+//
+
+#ifndef Node_h
+#define Node_h
+
+#include <string>
+using std::string;
+
+class Node
+{
+	friend class StockAccount;
+public:
+	Node(string& name, double number)
+		:stock_symbol(name), stock_number(number)
+	{
+		this->left = NULL;
+		this->right = NULL;
+	}
+private:
+	string stock_symbol;
+	double stock_number;
+	Node *left;	    // previous pointer
+	Node *right;	// next pointer
+
+};
+
+#endif
